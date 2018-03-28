715221a851c1a3301a332b918bc1435d98c95922
hbwhyte <heather.b.whyte@gmail.com>
2018-03-28 16:03:34 +0800

    Update dependencies and clean up twitter search

--- src/main/java/twitter/monzo/mappers/MonzoMapper.java
+++
@@ -1,8 +0,0 @@
-package twitter.monzo.mappers;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MonzoMapper {
-
-}
--- src/main/java/twitter/monzo/services/MonzoService.java
+++ src/main/java/twitter/monzo/services/MonzoService.java
@@ -1,50 +1,58 @@
 package twitter.monzo.services;
 
-import twitter4j.Twitter;
-import twitter.monzo.mappers.MonzoMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import twitter.monzo.model.external.SearchResult;
-import twitter.monzo.model.internal.MonzoResponse;
 import twitter4j.*;
+import twitter4j.Twitter;
 import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.json.DataObjectFactory;
 
-import java.time.LocalDate;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MonzoService {
-    @Autowired
-    RestTemplate restTemplate;
 
-    @Autowired
-    MonzoMapper monzoMapper;
-
-    public static List<Status> searchTwitter(String search) {
+    // Searches 10 most recent tweets for a search term and returns a JSON Object
+    public static JSONObject searchTwitter(String search) {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setJSONStoreEnabled(true);
+        Twitter twitter = new TwitterFactory().getInstance();
+        Query query = new Query(search);
+        query.setResultType(Query.ResultType.recent);
+        query.count(10);
 
-        Twitter twitter = new TwitterFactory(cb.build()).getInstance();
-        List<Status> tweets = null;
+        QueryResult result = null;
+
+        try {
+            result = twitter.search(query);
+
+            RateLimitStatus rateLimit = twitter.getRateLimitStatus().get("/users/search");
+            System.out.println("My rate limit status" + rateLimit);
+
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        }
+        JSONObject jsonResults = new JSONObject(result);
+        System.out.println(jsonResults);
+
+        return jsonResults;
+    }
+
+    // Searches 10 most recent tweets for a search term and returns a String List
+    public static List<String> searchTwitterList(String search) {
+        Twitter twitter = new TwitterFactory().getInstance();
+        List<String> tweets = null;
         try {
             Query query = new Query(search);
             query.setResultType(Query.ResultType.recent);
             query.count(10);
             QueryResult result = twitter.search(query);
-            for (Status tweet : result.getTweets()) {
-                String json = TwitterObjectFactory.getRawJSON(tweet);
-                System.out.println(json);
-            }
-//            System.exit(0); //Shuts down API after search.
-//            do {
-//                SearchResult obj = new SearchResult();
-//                result = twitter.search(query);
-//                tweets = result.getTweets();
-//                System.out.println(result.getTweets());
-//            } while ((query = result.nextQuery()) != null);
-
+            tweets = result.getTweets().stream().map(item -> item.getText()).collect(Collectors.toList());
         } catch (TwitterException te) {
             te.printStackTrace();
             System.out.println("Failed to search tweets: " + te.getMessage());
@@ -53,8 +61,7 @@ public class MonzoService {
         return tweets;
     }
 
-    // TODO: Instead of printing the JSON to the console I need to map the json to SearchResults and return parsed json
-
+    // Returns tweets from my timeline
     public List<Status> getMyTimeLine() throws TwitterException {
         List<Status> statuses = null;
         try {
@@ -70,9 +77,19 @@ public class MonzoService {
         return statuses;
     }
 
-}
+    // Posts a tweet to my user (@randomJavaFun)
+    public String createTweet(String tweet) {
+        Twitter twitter = new TwitterFactory().getInstance();
+        Status status = null;
+        try {
+            status = twitter.updateStatus(tweet);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to post tweet: " + te.getMessage());
+            System.exit(-1);
+        }
+        return status.getText();
+    }
 
-//    public MonzoResponse searchTwitter(String searchTerm) {
-//        String fQuery = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=realDonaldTrump&count=2";
-//        return response = restTemplate.getForObject(fQuery, MonzoResponse.class);
-//    }
+
+}
