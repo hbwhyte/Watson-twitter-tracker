d85c03f9e5b10855feee4eedfbbe677eb929fb9f
hbwhyte <heather.b.whyte@gmail.com>
2018-05-10 17:45:17 +0700

    Complete TwitterService Junit tests

---
+++ src/test/java/twitter/services/TwitterServiceTest.java
@@ -0,0 +1,25 @@
+package twitter.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class TwitterServiceTest {
+
+    TwitterService twitterService;
+
+    @Before
+    public void setUp() {
+       twitterService = new TwitterService();
+    }
+
+    @Test
+    public void encodeHashtags() throws Exception {
+        String tweet = "RT @randomJavaFun #codingnomads";
+        String expected = "RT+randomJavaFun+%23codingnomads";
+        assertEquals(expected,twitterService.encodeHashtags(tweet));
+    }
+}
